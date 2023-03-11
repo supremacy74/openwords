1f54d0b3305156c79735f46e2a2990ec0289da8d
unknown <supremacy318@gmail.com>
2023-03-12 01:53:19 +0600

    some 30

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -7,8 +7,6 @@ import HeadingInterface from '@/interfaces/HeadingInterface'
 interface Props extends HeadingInterface {}
 
 const Heading: React.FC<Props> = ({ priority, children }) => {
-    // const Tag = `h${priority}` as keyof JSX.IntrinsicElements
-
     return <h1 className={`${styles.heading} ${styles['h1']}`}>{children}</h1>
 }
 
